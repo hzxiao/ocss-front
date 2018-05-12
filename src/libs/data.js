795f2afd3d1f4aa77c7da3a5c74c7b5cdcb861e8
Xiaozh <838223935@qq.com>
2018-05-12 14:18:34 +0800

    [mod] 修改增加选课逻辑

--- src/libs/data.js
+++ src/libs/data.js
@@ -1,5 +1,25 @@
-export const attrList = ["人文科学类","自然科学类","社会科学类","工程技术类","教师教育类","艺术体育类"]
+export const attrList = ["人文科学类", "自然科学类", "社会科学类", "工程技术类", "教师教育类", "艺术体育类"]
 
-export const natureList = ["通识类选修课程","精品通识选修课程","教师教育类课程(理论)","教师教育类课程(技能)"];
+export const natureList = ["通识类选修课程", "精品通识选修课程", "教师教育类课程(理论)", "教师教育类课程(技能)"];
 
 export const campusList = ['大学城', '桂花岗'];
+
+export const schoolYearList = [{
+        id: "2017-2018",
+        name: "2017-2018学年"
+    },
+    {
+        id: "2018-2019",
+        name: "2018-2019学年"
+    }
+];
+
+export const termList = [{
+        id: "1",
+        name: "第一学期"
+    },
+    {
+        id: "2",
+        name: "第二学期"
+    }
+]
